6e7f0be5ffaf7c72092ba0aa5f2a0c95438a99eb
Jörg Krause <joerg@isageek.onmicrosoft.com>
2026-05-06 16:34:09 +0200

    Test case

--- Tests/Workshop.UnitTests/BusinessLogicTests.cs
+++ Tests/Workshop.UnitTests/BusinessLogicTests.cs
@@ -1,19 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Workshop.BusinessLogicLayer;
+using Workshop.DataAccessLayer;
+using Workshop.DomainModels;
 
 namespace Workshop.UnitTests;
 
 [TestClass]
 public sealed class BusinessLogicTests
 {
+
+  // The Microsoft way!
+  private static IServiceProvider BuildProvider(string dbName)
+  {
+    var services = new ServiceCollection();
+    services.AddDbContext<WorkshopContext>(options => options.UseInMemoryDatabase(dbName));
+    return services.BuildServiceProvider();
+  }
+
+
   [TestMethod]
-  public void CustomerManager_GetAllCustomers_ReturnsAllCustomers()
+  public async Task CustomerManager_GetAllCustomers_ReturnsAllCustomers()
   {
     // Setup
     // Mock for IServiceProvider
-    //var customerManager = new CustomerManager(??);
+    var serviceProvider = BuildProvider("TestDb");
+    var seed = serviceProvider.GetRequiredService<WorkshopContext>();
+    seed.Customers.Add(new Customer { Number = "A0001", Name = "Alice" });
+    seed.Customers.Add(new Customer { Number = "A0002", Name = "Bob" });
+    await seed.SaveChangesAsync();
+
+    var customerManager = new CustomerManager(serviceProvider);
     // Execute
+    var result = await customerManager.GetAllCustomers();
 
     // Verify
+    Assert.AreEqual(2, result.Count());
+
   }
 
   [TestMethod]
